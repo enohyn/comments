09ef321dca4130c633442ccc9f3c674d0e72c57d
Shayok Ullah <35142553+enohyn@users.noreply.github.com>
2022-12-05 13:03:56 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomByte, randomBytes } = require('crypto');
+const app = express();
+const cors = require('cors');
+app.use(bodyParser.json());
+app.use(cors());
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments',(req,res)=>{
+    res.send(commentsByPostId[req.params.id]);
+});
+app.post('/posts/:id/comments',(req,res)=>{
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body
+    const comments = commentsByPostId[req.params.id] || []
+    comments.push({id: commentId, content});
+    commentsByPostId[req.params.id]=comments;
+    res.status(201).send(comments);
+});
+
+app.listen(4001, ()=>{
+    console.log('Hit from 4001');
+});
