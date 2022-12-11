a2851878297c26fc19efc7153b1f6a7fdd9d705b
enohyn <shayokullah@gmail.com>
2022-12-11 17:07:09 +0600

    [fix] sending empty array for get method

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ app.use(bodyParser.json());
 app.use(cors());
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments',(req,res)=>{
-    res.send(commentsByPostId[req.params.id]);
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
 });
-app.post('/posts/:id/comments',(req,res)=>{
+app.post('/posts/:id/comments', (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body
     const comments = commentsByPostId[req.params.id] || []
-    comments.push({id: commentId, content});
-    commentsByPostId[req.params.id]=comments;
+    comments.push({ id: commentId, content });
+    commentsByPostId[req.params.id] = comments;
     res.status(201).send(comments);
 });
 
-app.listen(4001, ()=>{
+app.listen(4001, () => {
     console.log('Hit from 4001');
 });
